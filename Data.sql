5e92368306a6cbe40fb06ba66c343acc58dfe472
Abdulrahman <boodynaser@rocketmail.com>
2021-04-19 21:51:33 +0200

    Quering using WHERE and ORDER by

--- Data.sql
+++ Data.sql
@@ -1,7 +1,13 @@
-CREATE TABLE Books (id INTEGER ,name TEXT ,rating INTEGER);
+CREATE TABLE Books (id INTEGER ,name TEXT ,rating INTEGER,release_year INTEGER);
 
-INSERT INTO books VALUES (1,"Harry Potter", 5);
-INSERT INTO books VALUES (2,"Cinderlla",4);
-INSERT INTO books VALUES (3,"King Lear",3.5);
+INSERT INTO books VALUES (1,"Harry Potter", 10,1994);
+INSERT INTO books VALUES (2,"Cinderlla",7,1980);
+INSERT INTO books VALUES (3,"Prince and the Poor",8,1960);
+INSERT INTO books VALUES (3,"King Lear",7,1995);
+INSERT INTO books VALUES (3,"ALice in the Wonderland",9.5,2000);
+INSERT INTO books VALUES (3,"Snow White",5,1988);
 
+SELECT * FROM books; /*Quereing the table*/
 SELECT * FROM books where rating >= 4 ;
+SELECT * FROM books ORDER by release_year;
+SELECT * FROM books WHERE rating>=6 ORDER by release_year;
