5a24216243ae2a385e588412726d1d0c8937bbf8
Abdulrahman <boodynaser@rocketmail.com>
2021-04-19 23:07:36 +0200

    Add more entries to the table

--- Data.sql
+++ Data.sql
@@ -1,11 +1,24 @@
-CREATE TABLE Books (id INTEGER ,name TEXT ,rating INTEGER,release_year INTEGER);
+CREATE TABLE Books (id INTEGER ,name TEXT ,rating INTEGER,release_year INTEGER,genre TEXT,price INTEGER);
 
-INSERT INTO books VALUES (1,"Harry Potter", 10,1994);
-INSERT INTO books VALUES (2,"Cinderlla",7,1980);
-INSERT INTO books VALUES (3,"Prince and the Poor",8,1960);
-INSERT INTO books VALUES (3,"King Lear",7,1995);
-INSERT INTO books VALUES (3,"ALice in the Wonderland",9.5,2000);
-INSERT INTO books VALUES (3,"Snow White",5,1988);
+INSERT INTO books VALUES (1,"Harry Potter", 10,1994,"Fantasy",12);
+INSERT INTO books VALUES (2,"Cinderlla",7,1980,"Fairy tale",13);
+INSERT INTO books VALUES (3,"Prince and the Pauper",8,1960,"Children's literature‎
+",20);
+INSERT INTO books VALUES (4,"King Lear",7,1995,"Drama",10);
+INSERT INTO books VALUES (5,"ALice in the Wonderland",9.5,2000,"Fantasy",8);
+INSERT INTO books VALUES (6,"Snow White",5,1988,"Fairy tale",30);
+
+INSERT INTO books VALUES (7,"David Copperfeld", 10,1996,"Fantasy",12);
+INSERT INTO books VALUES (8,"Hercules",7,2001,"Fairy tale",13);
+INSERT INTO books VALUES (9,"Pinochio",8,1950,"Children's literature‎
+",20);
+INSERT INTO books VALUES (10,"The Pyramids",7,2005,"Drama",10);
+INSERT INTO books VALUES (11,"The Nile",9.5,2010,"Fantasy",8);
+INSERT INTO books VALUES (12,"Snow White Second Edition",5,200,"Fairy tale",30);
+
+INSERT INTO books VALUES (13,"Hunde und Katzen",7,1995,"Drama",10);
+INSERT INTO books VALUES (14,"Die Schwarze Tigerin",9.5,2000,"Fantasy",8);
+INSERT INTO books VALUES (15,"Was für ein Tag",5,1988,"Fairy tale",30);
 
 SELECT * FROM books; /*Quereing the table*/
 SELECT * FROM books where rating >= 4 ;
